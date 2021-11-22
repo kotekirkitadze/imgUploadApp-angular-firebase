044e645a52ac1c9ae4e4babac2ab53c98afb87c8
kote1999 <kotekirkitadze@gmail.com>
2021-11-22 18:18:21 +0400

    scaffolding done: componentes and services generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    NavbarComponent,
+    LoginComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
